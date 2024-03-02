c19333af2cee706b30b2e97100e864ecc1670156
heitorfreiire <heitorbitcoin2021@gmail.com>
2024-03-01 21:41:40 -0300

    aaaa

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public  class Main {
+    public static void main (String[] args){
+        listaProduto("laranja");
+        listaProduto("banana");
+
+    }
+
+    public static void listaProduto(String nomeproduto){
+        System.out.println(nomeproduto);
+
+    }
+}
