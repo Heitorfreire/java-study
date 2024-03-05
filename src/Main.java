709d02af4d998dc413e742732ad41af172ed0fe0
Heitorfreire <heitorbitcoin2021@gmail.com>
2024-03-04 21:07:33 -0300

    aula3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,35 @@
-public  class Main {
-    public static void main (String[] args){
-        listaProduto("laranja");
-        listaProduto("banana");
+public class Main {
+    private String nome;
+    private double salarioBruto;
+    private double percentualDesconto;
 
+    public Main(String nome, double salarioBruto, double percentualDesconto) {
+        this.nome = nome;
+        this.salarioBruto = salarioBruto;
+        this.percentualDesconto = percentualDesconto;
     }
 
-    public static void listaProduto(String nomeproduto){
-        System.out.println(nomeproduto);
+    public String getNome() {
+        return nome;
+    }
 
+    public double getSalarioBruto() {
+        return salarioBruto;
+    }
+
+    public double getPercentualDesconto() {
+        return percentualDesconto;
+    }
+
+    public double calcularSalarioLiquido() {
+        double desconto = salarioBruto * (percentualDesconto / 100);
+        return salarioBruto - desconto;
+    }
+
+    public static void main(String[] args) {
+        Main funcionario = new Main("João", 2000.0, 10.0);
+        System.out.println("Nome: " + funcionario.getNome());
+        System.out.println("Salário Bruto: " + funcionario.getSalarioBruto());
+        System.out.println("Salário Líquido: " + funcionario.calcularSalarioLiquido());
     }
 }
